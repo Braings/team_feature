4cb385c63e4c4e5e8224bc504d6008b9e11b87d6
Kim-jiseong <kimjs030727@gmail.com>
2025-11-14 15:55:02 +0900

    test: Test Code
    - Duplicated User Test Code
    - failed Test (TODO: bug fix)

--- src/test/java/com/bridgeX/UserApiIntegrationTest.java
+++ src/test/java/com/bridgeX/UserApiIntegrationTest.java
@@ -1,7 +1,8 @@
 package com.bridgeX;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +17,53 @@ public class UserApiIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
+    
+    // Test02: User name Duplication
+    
+    @Test
+    void Username_Duplication() throws Exception {
+    	// first Register
+    	/*
+    	String signupJson1 = """
+    	        {
+    	          "username": "DuplicateUser",
+    	          "email": "test1@test.com",
+    	          "password": "1234"
+    	        }
+    	        """;
 
+    	    mockMvc.perform(
+    	            post("/api/sign")
+    	                    .contentType(MediaType.APPLICATION_JSON)
+    	                    .content(signupJson1)
+    	    )
+    	    .andExpect(status().isOk());
+    	*/
+    	    // second Register (Duplicate)
+        String signupJson2 = """
+            {
+              "username": "DuplicateUser",
+              "email": "test2@test.com",
+              "password": "5678"
+            }
+            """;
+
+        mockMvc.perform(
+                post("/api/sign")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signupJson2)
+        )
+        .andExpect(status().isBadRequest()) // Expect 400
+        .andExpect(jsonPath("$.message").value("이미 존재하는 아이디입니다.")); // UserService와 반환 string을 맞춰야 함
+    }
+
+    
+    
+    /*
+    
+    // Test01: User Register and Login
     @Test
     void signup_and_login() throws Exception {
-
         // 1) 회원가입 요청 JSON  (/api/sign)
         String signupJson = """
             {
@@ -53,4 +97,5 @@ public class UserApiIntegrationTest {
         .andExpect(jsonPath("$.success").value(true))
         .andExpect(jsonPath("$.message").value("로그인 성공")); // UserService와 string 맞추기
     }
+    */
 }
