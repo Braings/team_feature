4a014a01b1a8db05eabcd52b0d928f7a6e3f1e1c
taeyang03 <taeyangs666@gmail.com>
2025-11-14 15:03:17 +0900

    logIn

--- bridge-x/src/views/logIn.vue
+++ bridge-x/src/views/logIn.vue
@@ -1,10 +1,30 @@
 <template>
   <div>
     <div class="backbon">
-      <div class="logIn-Box">
-        <p class="title cc-font">B R I D G E - X</p>
-        <p class="logIn">Log In</p>
+      <div class="left-section">
+        <div class="content-wrapper">
+          <p class="subtitle">맞춤형 운동 추천 솔루션</p>
+          <h1 class="title cc-font" >B R I D G E - X</h1>
+          <p class="footer-text">Team Braings - 2025</p>
+        </div>
       </div>
+      <div class="right-section">
+        <div class="ID-Box">
+          <p class="ID-Text">ID</p>
+        </div>
+        <div class="LogInToYourAccount cc-font">Log In To Your Account</div>
+        <div class="PW-Box">
+          <p class="PW-Text">PASSWARD</p>
+        </div>
+
+        <div class="LogIn-Box">
+          <p class="LogIn-Text cc-font">LOG IN</p>
+        </div>
+
+        <div class="under-bar"></div>
+        <div class="makeAccount">New to Bridge-X? Sign up</div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -29,38 +49,150 @@ export default {
   }
 
   .backbon{
+    display: flex;
     width: 100vw;
     height: 100vh;
-    background-color: #c5e7d1;
-  }
-
-  .backbon .logIn-Box{
-    border-radius: 10%/15%;
-    position: fixed;
-    top: 10vh;
-    right: 20vw;
-    width: 60vw;
-    height: 70vh;
+    font-family: sans-serif; /* 기본 폰트 설정 */
+    overflow: hidden;
     background-color: white;
   }
 
-  .logIn-Box .title{
-    font-size: 5vw;
-    font-weight: bold;
-    margin: 0;
-    line-height: 5vh;
+  // 왼쪽 섹션
+  .left-section {
+    width: 50vw;
+    background-color: #1a1a1a;
+    color: white;
     position: relative;
-    left: 13vw;
-    top: 10vh;
+    background-image: url('../img/gym.png');
+    background-size: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+
   }
 
-  .logIn-Box .logIn{
-    font-size: 3vw;
+  .left-section .content-wrapper {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    padding: 0 3vw 3vh 3vw;
+  }
+
+  .left-section .subtitle {
+    font-size: 1.2vw;
+    margin-bottom: 0.5vh;
+  }
+
+  .left-section .title {
+    font-size: 2.5vw;
     font-weight: bold;
     margin: 0;
     line-height: 5vh;
-    position: relative;
-    left: 45vw;
-    top: 45vh;
   }
+
+  .left-section .footer-text {
+    font-size: 0.8vw;
+    margin-top: 5vh;
+  }
+
+  // 오른쪽 세션
+
+  .right-section{
+    height: 100vh;
+    width: 50vw;
+    background-color: white;
+    position: relative;
+
+  }
+  .ID-Box{
+    border-radius: 0;
+    position: absolute;
+    width: 30vw;
+    height: 10vh;
+
+    top: 27vh;
+    right: 10vw;
+
+    background-color: #f3f3f3;
+    box-shadow: 2px 2px 5px black;
+  }
+
+  .ID-Box .ID-Text{
+    position: absolute;
+    top: 1vh;
+    left: 0.5vw;
+    font-size: 1vw;
+    color: black;
+    padding: 0.5vh 0.5vw;
+  }
+
+  .PW-Box{
+    border-radius: 0;
+    position: absolute;
+    width: 30vw;
+    height: 10vh;
+
+    top: 40vh;
+    right: 10vw;
+
+    background-color: #f3f3f3;
+    box-shadow: 2px 2px 5px black;
+  }
+
+  .PW-Box .PW-Text{
+    position: absolute;
+    top: 1vh;
+    left: 0.5vw;
+    font-size: 1vw;
+    color: black;
+    padding: 0.5vh 0.5vw;
+  }
+
+  .LogIn-Box{
+    border-radius: 0;
+    position: absolute;
+    width: 30vw;
+    height: 5vh;
+
+    top: 53vh;
+    right: 10vw;
+
+    background-color: #252525;
+    box-shadow: 2px 2px 5px black;
+  }
+
+  .LogIn-Box .LogIn-Text{
+    text-align: center;
+    font-size: 1.5vw;
+    color: #89FFB1;
+    padding: 0.5vh 0.5vw;
+  }
+
+  .makeAccount {
+    position: absolute;
+    top: 60vh;
+    left: 18.5vw;
+    font-size: 1vw;
+    color: black;
+    padding: 0.5vh 0.5vw;
+  }
+
+  .LogInToYourAccount {
+    position: absolute;
+    top: 15vh;
+    left: 13vw;
+    font-size: 2vw;
+    color: black;
+    padding: 0.5vh 0.5vw;
+  }
+
+  .under-bar {
+    justify-content: bottom;
+    width: 30vw;
+    height: 2vh;
+    background-color: #252525;
+    position: absolute;
+    right: 10vw;
+    top: 90vh;
+  }
+
 </style>
